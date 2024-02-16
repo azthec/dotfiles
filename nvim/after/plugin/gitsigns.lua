85bb11ef0aadcb1cb62d567ff50b379b5757fd84
Rui Balau <rui.balau@lunatech.nl>
2024-02-16 10:40:32 +0100

    #nvim git indicator navigation

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -1,6 +1,18 @@
 local gs = require('gitsigns')
+
+vim.keymap.set('n', ']c', function()
+  if vim.wo.diff then return ']c' end
+  vim.schedule(function() gs.next_hunk() end)
+  return '<Ignore>'
+end, { expr = true })
+
+vim.keymap.set('n', '[c', function()
+  if vim.wo.diff then return '[c' end
+  vim.schedule(function() gs.prev_hunk() end)
+  return '<Ignore>'
+end, { expr = true })
+
 vim.keymap.set('n', '<leader>hb', gs.toggle_current_line_blame)
 vim.keymap.set('n', '<leader>hp', gs.preview_hunk)
 vim.keymap.set('n', '<leader>hr', gs.reset_hunk)
 vim.keymap.set('n', '<leader>hd', gs.diffthis)
-
