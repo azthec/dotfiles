8ac1cd0f8ca1d72d54af6ffc004fe96b13076cb8
azthec <azthec@users.noreply.github.com>
2024-07-26 11:09:26 +0200

    #nvim telescope multi select open files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -10,14 +10,40 @@ vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
 -- drop-down menu for vim spellchecks
 vim.keymap.set('n', '<leader>fs',
   function() require('telescope.builtin').spell_suggest(require('telescope.themes').get_cursor({})) end)
+
+-- multiselect open one buffer per file
+local open_selected = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local selected = picker:get_multi_selection()
+  if vim.tbl_isempty(selected) then
+    actions.select_default(prompt_bufnr)
+  else
+    actions.close(prompt_bufnr)
+    for _, file in pairs(selected) do
+      if file.path then vim.cmd('edit' .. (file.lnum and ' +' .. file.lnum or '') .. ' ' .. file.path) end
+    end
+  end
+end
+local open_all = function(prompt_bufnr)
+  actions.select_all(prompt_bufnr)
+  open_selected(prompt_bufnr)
+end
+
 telescope.setup({
   defaults = {
     path_display = { 'truncate ' },
     mappings = {
       i = {
-        ['<C-k>'] = actions.move_selection_previous, -- move to prev result
-        ['<C-j>'] = actions.move_selection_next,     -- move to next result
-        ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+        ['<C-k>']  = actions.move_selection_previous, -- move to prev result
+        ['<C-j>']  = actions.move_selection_next,     -- move to next result
+        ['<C-q>']  = actions.send_selected_to_qflist + actions.open_qflist,
+        ['<CR>']   = open_selected,
+        ['<C-CR>'] = open_all,
+      },
+      n = {
+        ['<c-d>']  = actions.delete_buffer,
+        ['<CR>']   = open_selected,
+        ['<C-CR>'] = open_all,
       },
     },
   },
